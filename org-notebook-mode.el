cdc916b1c5e6166808b23125128021347737d9c3
liamst19 <50284753+liamst19@users.noreply.github.com>
2019-07-01 21:45:38 -0700

    Window with subtree opens; README.org added.

---
+++ org-notebook-mode.el
@@ -0,0 +1,43 @@
+;;; org-notebook minor-mode
+
+(make-variable-buffer-local
+ (defvar org-notebook-mode-on nil
+   "Toggle org notebook view mode"))
+
+(defun org-toggle-notebook-view ()
+  "Toggles Notebook view mode"
+  (interactive)
+  (if org-notebook-mode-on
+      (progn
+        (setq org-notebook-mode-on nil)
+        (let ((ind-buf "org-subtree-indirect"))
+          (delete-window (get-buffer-window ind-buf))
+          (kill-buffer ind-buf)))
+    (setq org-notebook-mode-on t)))
+
+;; Indirect orgtree, reuse buffer
+(defun org-subtree-to-indirect-buffer ()
+  "Opens the subtree in a new indirect buffer."
+  (interactive)
+  (if (and org-notebook-mode-on (org-at-heading-p))
+      (let ((ind-buf "org-subtree-indirect"))
+        (if (get-buffer-window ind-buf)
+            (kill-buffer ind-buf)
+          (split-window nil nil 'right))
+        (clone-indirect-buffer-other-window ind-buf t)
+        (org-narrow-to-subtree)
+        (outline-show-entry)
+        (show-children)
+        (windmove-left))))
+
+;;;###autoload
+(define-minor-mode org-notebook-mode
+  "Toggle org notebook view mode"
+  :init-value nil
+  :lighter " notebook"
+   (add-hook 'post-command-hook 'org-subtree-to-indirect-buffer))
+
+;;;###autoload
+(add-hook 'org-mode-hook 'org-notebook-mode)
+
+(provide 'org-notebook-mode)
