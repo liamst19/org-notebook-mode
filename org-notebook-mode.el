cd2c64ee18050a36b0596b8bb999cf347b2e8f15
liamst19 <50284753+liamst19@users.noreply.github.com>
2019-07-02 13:59:31 -0700

    Window direction, README

--- org-notebook-mode.el
+++ org-notebook-mode.el
@@ -8,16 +8,47 @@
  (defvar org-notebook-mode-buffer nil
    ""))
 
-(defun org-toggle-notebook-view ()
-  "Toggles Notebook view mode"
+(make-variable-buffer-local
+ (defvar org-notebook-mode-subtree-buffer nil
+   ""))
+
+(defvar org-notebook-window-direction 'right
+  "The direction of org-notebook window will be created.")
+
+(defun org-notebook-move-back ()
+  "Switch back to the original buffer window from the subtree buffer window."
+  ;; I wonder if there is another way to do this, where
+  ;; the window with the original buffer can be specified.
+  (cond ((eq org-notebook-window-direction 'right)
+         (windmove-left))
+        ((eq org-notebook-window-direction 'left)
+         (windmove-right))
+        ((eq org-notebook-window-direction 'above)
+         (windmove-down))
+        ((eq org-notebook-window-direction 'below)
+         (windmove-up))))
+
+(defun org-notebook-toggle-view ()
+  "Toggles org-notebook view mode"
   (interactive)
   (if org-notebook-mode-on
+      ;; Turn off
       (progn
         (setq org-notebook-mode-on nil)
-        (let ((ind-buf "org-subtree-indirect"))
-          (delete-window (get-buffer-window ind-buf))
-          (kill-buffer ind-buf)))
-    (setq org-notebook-mode-on t)))
+        ;; Get rid of the subtree window
+        (delete-window (get-buffer-window org-notebook-mode-subtree-buffer))
+        ;; Get rid of the subtree buffer
+        (kill-buffer org-notebook-mode-subtree-buffer))
+    ;; If view mode is off, turn on
+    (progn
+      (setq org-notebook-mode-on t))))
+
+(defun org-notebook-clone-subtree-other-buffer ()
+  "Clones original buffer to the new window, and show and expand only the subtree."
+  (clone-indirect-buffer-other-window org-notebook-mode-subtree-buffer t)
+  (org-narrow-to-subtree)
+  (outline-show-entry)
+  (show-children))
 
 ;; Indirect orgtree, reuse buffer
 (defun org-subtree-to-indirect-buffer ()
@@ -25,25 +56,33 @@
   (interactive)
   (if org-notebook-mode-on
       (progn
+        ;; Make sure current buffer is the original and not the subtree
         (if (eq org-notebook-mode-buffer (current-buffer))
             (progn
-              (let ((ind-buff (concat (buffer-name) "-subtree")))
-                (if (org-at-heading-p)
-                    (progn
-                      (if (get-buffer-window ind-buff)
-                          (kill-buffer ind-buff)
-                        (split-window nil nil 'right))
-                      (clone-indirect-buffer-other-window ind-buff t)
-                      (org-narrow-to-subtree)
-                      (outline-show-entry)
-                      (show-children)
-                      (windmove-left)))))))))
+              (setq org-notebook-mode-subtree-buffer (concat (buffer-name) "-subtree"))
+              ;; Subtree opens only when cursor is at an org-header
+              (if (org-at-heading-p)
+                  (progn
+                    ;; If subtree window is open, get rid of the subtree content
+                    (if (get-buffer-window org-notebook-mode-subtree-buffer)
+                        (kill-buffer org-notebook-mode-subtree-buffer)
+                      ;; If subtree window is not open, split window
+                      ;; in the specified direction.
+                      (split-window nil nil org-notebook-window-direction))
+                    ;; Generate content in the subtree buffer
+                    (org-notebook-clone-subtree-other-buffer)
+                    ;; Move back to the original buffer
+                    (org-notebook-move-back)
+                    )))))))
 
 ;;;###autoload
 (define-minor-mode org-notebook-mode
-  "Toggle org notebook view mode"
+  "Minor mode for quickly browsing through org-mode notes."
   :init-value nil
   :lighter " notebook"
+  :keymap (let ((map  (make-sparse-keymap)))
+            (define-key map (kbd "C-c o") 'org-notebook-toggle-view)
+            map)
   (add-hook 'post-command-hook 'org-subtree-to-indirect-buffer)
   (setq org-notebook-mode-buffer (current-buffer)))
 
